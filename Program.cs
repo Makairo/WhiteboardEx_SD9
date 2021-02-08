1fd75944cf9b953c3f8a1434477cca3f069b6b99
Andrew Hejl <andrew.hejl@outlook.com>
2021-02-08 10:49:37 -0800

    Update.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace WhiteboardEx_SD9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Whiteboard solutions container.");
+        }
+
+    }
+}
